313992b69d65718d954718e8d81fa8d1d52b3dd6
Himanshu Sharma <samitihs4@gmail.com>
2019-06-13 00:33:25 +0530

    Send frames and receiving it at Flask server
    Using XMLHttpRequest to send frame data to Flask server and receiving the same on Flask....

--- web_main.py
+++ web_main.py
@@ -1,13 +1,21 @@
-from flask import request
+from flask import request, Flask
 from flask_ngrok import run_with_ngrok
 
 app=Flask(__name__)
-run_with_ngrok(app)
+# ngrok used for tunneling the network
+# run_with_ngrok(app)
 
+# index function execute when traffic occur at '/'
 @app.route("/", methods=['POST','GET'])
 def index():
-		return "Hello world"
-
+	if request.method == 'POST':
+		# reading the image (byte stream)
+		data=request.stream.read()
+		# seperating data from the header {data:image/png;....}
+		data=str(data).split(',')[1]
+		# print to be sure we are receiving the data correctly
+		print(data)
+		return ""
 
 if __name__ == "__main__":
 	app.run()
