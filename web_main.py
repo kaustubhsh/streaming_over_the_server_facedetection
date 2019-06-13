da09e52d4ca19c641eab5af3784b62c4e0b3c5cc
Himanshu Sharma <samitihs4@gmail.com>
2019-06-13 10:21:32 +0530

    Decoding image data and saving to a folder
    Data received from html is converted to numpy format - understandable by python opencv

--- web_main.py
+++ web_main.py
@@ -1,10 +1,13 @@
 from flask import request, Flask
 from flask_ngrok import run_with_ngrok
+import base64
+import cv2
+import numpy as np
 
 app=Flask(__name__)
 # ngrok used for tunneling the network
 # run_with_ngrok(app)
-
+count=0
 # index function execute when traffic occur at '/'
 @app.route("/", methods=['POST','GET'])
 def index():
@@ -14,8 +17,26 @@ def index():
 		# seperating data from the header {data:image/png;....}
 		data=str(data).split(',')[1]
 		# print to be sure we are receiving the data correctly
-		print(data)
+		# print(data)
+
+		global count
+		count+=1
+		
+		''' starting conversion of html-base64 data to cv-numpy array '''
+		# decoding base64 data
+		img = base64.b64decode(data)
+		# generating numpy array from decoded data
+		npimg = np.frombuffer(img, dtype=np.uint8)
+		# generating 3d array from 1d
+		source = cv2.imdecode(npimg, 1)
+		''' conversion finished '''
+
+		''' saving the image to the local images folder
+		to see that conversion is successful '''
+		cv2.imwrite('images\\hello'+str(count)+'.jpg', source)
+		
 		return ""
+
 
 if __name__ == "__main__":
 	app.run()
